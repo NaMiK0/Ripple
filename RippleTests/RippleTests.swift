6b554cd29dfbbca341f3a1169ce8f1258257f1b3
Kitolov <nikita.khaziev@mail.ru>
2026-04-16 15:12:21 +0300

    Initial Commit

---
+++ RippleTests/RippleTests.swift
@@ -0,0 +1,17 @@
+//
+//  RippleTests.swift
+//  RippleTests
+//
+//  Created by Nikita on 16.04.2026.
+//
+
+import Testing
+@testable import Ripple
+
+struct RippleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
